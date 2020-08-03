4d75077503c0234137601e03b4be8c0addd2cc84
Sojeong <heymango7393@gmail.com>
2020-08-04 04:33:08 +0900

    Updates

---
+++ precache-manifest.dc19ca2ffa34ab0e654e828b5514fa45.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3726ec92c8ec33c327768a66e9755d36",
+    "url": "/react_movie_app/index.html"
+  },
+  {
+    "revision": "cf3502a6f308fae234c0",
+    "url": "/react_movie_app/static/css/main.37707cbb.chunk.css"
+  },
+  {
+    "revision": "99da0de520a8e9335a5d",
+    "url": "/react_movie_app/static/js/2.e43a604c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_movie_app/static/js/2.e43a604c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf3502a6f308fae234c0",
+    "url": "/react_movie_app/static/js/main.58e108b8.chunk.js"
+  },
+  {
+    "revision": "b2eab6cde90deae0fa53",
+    "url": "/react_movie_app/static/js/runtime-main.e749ff8d.js"
+  },
+  {
+    "revision": "3180c7b72cccc08801530a501ad68863",
+    "url": "/react_movie_app/static/media/loader.3180c7b7.png"
+  }
+]);
